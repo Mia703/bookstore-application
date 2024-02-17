46f465b7b068119a8125d6771c45b0d539451363
jaina805 <158683992+jaina805@users.noreply.github.com>
2024-02-17 17:39:53 -0500

    Removed content from My Acconut and My Library due to error
    I removed content from my account and my library due to the errors that I kept receiving on the terminal and in the local host.... not sure what I'm doing wrong?

--- src/app/pages/my-library/page.js
+++
@@ -1,9 +0,0 @@
-import Navigation from "../../components/mainNavigation";
-import Footer from "../../components/Footer";
-import "./search.css";
-
-export default function MyLibrary () {
-	return(
-		<div id="my-library">hello library</div>
-	);
-}
