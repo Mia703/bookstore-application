d91b63b77986afa1bd2676830dfe0ac91e4528e2
jaina805 <158683992+jaina805@users.noreply.github.com>
2024-02-18 09:00:59 -0500

    Added sidebar to My Account

--- src/app/pages/my-account/page.js
+++ src/app/pages/my-account/page.js
@@ -7,10 +7,16 @@ export default function Account () {
         <div id="account-page">
 			<Navigation />
             <section className="account-section">
+                <div className="sidebar">
+                    <a href="#my-account">My Account</a>
+                    <a href="#profile">Profile</a>
+                    <a href="#password">Password</a>
+                    <a href="#settings">Settings</a>
+                </div>
                 <div className="container">
                     <h1>Welcome, (Name)</h1>                
                     <img src="https://i.pinimg.com/736x/59/cf/f9/59cff97550efef9af0940d1f9e33652a.jpg" alt="profile image" className="image"></img>
-                    <p>From your account dashboard, you can edit your <a href="#profile-link">profile details</a> and <a href="#password-link">edit your password</a>.</p>
+                    <p>From your account dashboard, you can edit your <a href="#profile">profile details</a> and <a href="#password">edit your password</a>.</p>
                     <p className="sign-out">Not (Name)? <a href="#sign-out-link">Sign out</a></p>
                 </div>
             </section>
