f984b1149d8feef2799b3b6ae3983347d8f01a22
jaina805 <158683992+jaina805@users.noreply.github.com>
2024-02-18 12:14:57 -0500

    Made changes to My Account page

--- src/app/pages/my-account/page.js
+++ src/app/pages/my-account/page.js
@@ -16,7 +16,21 @@ export default function Account () {
                 <div className="container">
                     <h1>Welcome, (Name)</h1>                
                     <img src="https://i.pinimg.com/736x/59/cf/f9/59cff97550efef9af0940d1f9e33652a.jpg" alt="profile image" className="image"></img>
-                    <p>From your account dashboard, you can edit your <a href="#profile">profile details</a> and <a href="#password">edit your password</a>.</p>
+                    <div className="dashboard">
+                        <div className="book-stat-card">
+                            <p>Total # of books started</p>
+                        </div>
+                        <div className="book-stat-card">
+                            <p>Total # of books finished</p>
+                        </div>
+                        <div className="book-stat-card">
+                            <p>Average rating</p>
+                        </div>
+                        <div className="book-stat-card">
+                            <p>Favorite genre</p>
+                        </div>
+                    </div>
+                    <p className="sign-out">From your account dashboard, you can edit your <a href="#profile">profile details</a> and <a href="#password">edit your password</a>.</p>
                     <p className="sign-out">Not (Name)? <a href="#sign-out-link">Sign out</a></p>
                 </div>
             </section>
