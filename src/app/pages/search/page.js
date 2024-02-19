e9c204848bd3d60b9200c25ceb464f21eb003b24
Amya Moore <bookworm7572@gmail.com>
2024-02-19 10:22:02 -0700

    Updated Search Page
    Added final styling to search form and filters form

--- src/app/pages/search/page.js
+++ src/app/pages/search/page.js
@@ -1,3 +1,4 @@
+'use client'
 import Navigation from "../../components/mainNavigation";
 import Book from "../../components/searchBooks";
 import Footer from "../../components/Footer";
@@ -9,60 +10,169 @@ export default function Search() {
 			<Navigation />
 			<section className="search-section">
 				<h1>Browse All Books</h1>
-				<form action="#">
-					<label htmlFor="Search"></label>
+				<form action="#" className="search-form">
+					<label htmlFor="search"></label>
 					<input
 						type="search"
 						name="search"
 						id="search"
-						placeholder="Search for Title or Author"
+						placeholder="Search for a Title or Author"
 					/>
-					<button type="submit">Search</button>
+					<button type="submit" className="search-button">
+						<span class="search-icon material-symbols-outlined">search</span>
+					</button>
 				</form>
+
 				<div className="filter-container">
-					<h2>Filters</h2>
-					<div className="filter-options">
-						<div className="category">
-							<p>Genres</p>
-									<input type="checkbox" name="genre" value="autobiography" /> Autobiography
-									<input type="checkbox" name="genre" value="biography" /> Biography
-									<input type="checkbox" name="genre" value="drama" /> Drama
-									<input type="checkbox" name="genre" value="fantasy" /> Fantasy
-									<input type="checkbox" name="genre" value="fiction" /> Fiction
-									<input type="checkbox" name="genre" value="historical fiction" /> Historical Fiction
-									<input type="checkbox" name="genre" value="horror" /> Horror
-									<input type="checkbox" name="genre" value="humor" /> Humor
-									<input type="checkbox" name="genre" value="mystery" /> Mystery
-									<input type="checkbox" name="genre" value="mythology" /> Mythology
-									<input type="checkbox" name="genre" value="nonfiction" /> Nonfiction
-									<input type="checkbox" name="genre" value="science fiction" /> Science Fiction
-									<input type="checkbox" name="genre" value="young adult" /> Young Adult
-						</div>		
-						<div className="category">
-							<p>Pace</p>
-								<input type="checkbox" name="pace" value="slow" /> Slow
-								<input type="checkbox" name="pace" value="medium" /> Medium
-								<input type="checkbox" name="pace" value="fast" /> Fast
-							</p>
-						</div>	
-						<div className="category">
-							<p>Pages</p>
-							<p>
-								<input type="checkbox" name="pages" value="lt300" /> Less than 300 pages
-							</p>
-							<p>
-								<input type="checkbox" name="pages" value="300-499" /> 300-499 pages
-							</p>
-							<p>
-								<input type="checkbox" name="pages" value="500plus" /> More than 500 pages
-							</p>
-						</div>	
+					<div className="container">
+						<h2>Filter</h2>
+						<button type="button" className="collapse-button">
+							<span class="material-symbols-outlined">add</span>
+							<span class="material-symbols-outlined">remove</span>
+						</button>
+					</div>
+					<div id="hidden">
+						<form action="#" className="filter-form">
+							<div className="filter-options">
+								<div className="category genre">
+									<p className="bold">Genres</p>
+									<input
+										type="checkbox"
+										name="genre"
+										value="autobiography"
+										id="autobiography"
+									/>
+									<label htmlFor="autobiography">Autobiography</label>
+									<br />
+									<input
+										type="checkbox"
+										name="genre"
+										value="biography"
+										id="biography"
+									/>
+									<label htmlFor="biography">Biography</label>
+									<br />
+									<input type="checkbox" name="genre" value="drama" id="drama" />
+									<label htmlFor="drama">Drama</label>
+									<br />
+									<input
+										type="checkbox"
+										name="genre"
+										value="fantasy"
+										id="fantasy"
+									/>
+									<label htmlFor="fantasy">Fantasy</label>
+									<br />
+									<input
+										type="checkbox"
+										name="genre"
+										value="fiction"
+										id="fiction"
+									/>
+									<label htmlFor="fiction">Fiction</label>
+									<br />
+									<input
+										type="checkbox"
+										name="genre"
+										value="historical fiction"
+										id="historical-fiction"
+									/>
+									<label htmlFor="historical-fiction">Historical Fiction</label>
+									<br />
+									<input type="checkbox" name="genre" value="horror" id="horror" />
+									<label htmlFor="horror">Horror</label>
+									<br />
+									<input type="checkbox" name="genre" value="humour" id="humour" />
+									<label htmlFor="humour">Humour</label>
+									<br />
+									<input
+										type="checkbox"
+										name="genre"
+										value="mystery"
+										id="mystery"
+									/>
+									<label htmlFor="mystery">Mystery</label>
+									<br />
+									<input
+										type="checkbox"
+										name="genre"
+										value="mythology"
+										id="mythology"
+									/>
+									<label htmlFor="mythology">Mythology</label>
+									<br />
+									<input
+										type="checkbox"
+										name="genre"
+										value="non-fiction"
+										id="non-fiction"
+									/>
+									<label htmlFor="non-fiction">Non-Fiction</label>
+									<br />
+									<input
+										type="checkbox"
+										name="genre"
+										value="science-fiction"
+										id="science-fiction"
+									/>
+									<label htmlFor="science-fiction">Science Fiction</label>
+									<br />
+									<input
+										type="checkbox"
+										name="genre"
+										value="young-adult"
+										id="young-adult"
+									/>
+									<label htmlFor="young-adult">Young Adult</label>
+									<br />
+								</div>
+								<div className="category pace">
+									<p className="bold">Pace</p>
+									<input type="checkbox" name="slow" value="slow" id="slow" />
+									<label htmlFor="slow">Slow</label>
+									<br />
+									<input type="checkbox" name="medium" value="medium" id="medium" />
+									<label htmlFor="medium">Medium</label>
+									<br />
+									<input type="checkbox" name="fast" value="fast" id="fast" />
+									<label htmlFor="fast">Fast</label>
+									<br />
+								</div>
+								<div className="category pages">
+									<p className="bold">Pages</p>
+									<input
+										type="checkbox"
+										name="less-than-300"
+										value="lt300"
+										id="lt300"
+									/>
+									<label htmlFor="lt300">Less than 300 pages</label>
+									<br />
+									<input
+										type="checkbox"
+										name="less-than-499"
+										value="lt499"
+										id="lt499"
+									/>
+									<label htmlFor="lt499">300 - 499 pages</label>
+									<br />
+									<input
+										type="checkbox"
+										name="more-than-500"
+										value="500plus"
+										id="500plus"
+									/>
+									<label htmlFor="500plus">More than 500 pages</label>
+									<br />
+								</div>
+							</div>
+							<button type="submit" className="bold">Apply Filters</button>
+						</form>
 					</div>
 				</div>
-				<button type="button">Apply</button>
-				<div className="books-container">
-					<Book />
-				</div>
+			</section>
+			<section className="books-section">
+				<Book />
 			</section>
 			<div className="back-to-top-button">
 				<button type="button">^</button>
