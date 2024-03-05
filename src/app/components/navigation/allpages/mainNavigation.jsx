2ce4dc251f1edae41289e5dd923d9bc129c9a644
Amya Moore <bookworm7572@gmail.com>
2024-03-05 09:02:45 -0700

    Update mainNavigation.jsx

--- src/app/components/navigation/allpages/mainNavigation.jsx
+++ src/app/components/navigation/allpages/mainNavigation.jsx
@@ -1,7 +1,23 @@
 import Link from "next/link";
+import app from "../../../api/firebase"
+import { getAuth, signOut } from "firebase/auth";
 import "./mainStyle.css";
 
 export default function Navigation () {
+	const signoutUser = () => {
+		const auth = getAuth(app);
+		signOut(auth)
+			.then(() => {
+				// Sign-out successful.
+				alert("Sign out was successful. Redirecting to login page.");
+				window.location.href="/pages/login"
+			})
+			.catch((error) => {
+				// An error happened.
+				alert("Error: Could not sign out user.\n" + error);
+			});
+	}
+
 	return (
 		<section className="navigation-section">
 			<div className="logo-container">
@@ -23,7 +39,8 @@ export default function Navigation () {
 						<Link href={"/pages/my-account"}>My Account</Link>
 					</li>
 					<li className="nav-item">
-						<Link href={"/pages/login"}>Log out</Link>
+						{/* <Link onClick={signoutUser}>Log out</Link> */}
+						<button type="button" onClick={signoutUser}>Log out</button>
 					</li>
 				</ul>
 			</nav>
